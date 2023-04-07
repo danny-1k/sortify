6cebfa32a6280ab1fdbce36580a2b2ba125d10c2
Daniel Ik <codingeinstein@gmail.com>
2023-04-07 10:57:24 +0000

    add data.py

---
+++ src/data.py
@@ -0,0 +1,45 @@
+import torch
+from torch.utils.data import Dataset
+from pipelines import AudioPipeline
+import librosa
+import pandas as pd
+import os
+from typing import Tuple
+import yaml
+from utils import split_waveform
+
+
+class AudioData(Dataset):
+    def __init__(self, train=True, data_dir="../data/raw", audio_dir="../data/audio", device="cuda"):
+        self.data_dir = data_dir
+        self.audio_dir = audio_dir
+        self.device = device
+
+        self.pipeline = AudioPipeline().to(device)
+
+        self.df = pd.read_csv(os.path.join(data_dir, "train.csv" if train else "test.csv"))
+
+        vals = yaml.safe_load(open("../config/data.yml", "r"))
+
+        self.max = vals["SPECTROGRAM_MAX"]
+        self.min = vals["SPECTROGRAM_MIN"]
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        row = self.df.iloc[idx]
+        track_id = row["id"]
+
+        waveform, sr = librosa.load(os.path.join(self.audio_dir, track_id) + ".mp3")
+
+        waveform = torch.from_numpy(waveform).to(self.device)
+
+        slices = split_waveform(waveform, sr=sr, secs_per_slice=3)
+
+        for slice in slices:
+            x = self.pipeline(slice, sr)
+
+            x = (x-self.min)/(self.max-self.min)
+
+            yield x
+
+    def __len__(self):
+        return len(self.df)
