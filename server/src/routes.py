35b8cf152a8d85ad0ea813af0798eefb459f76a1
Daniel Ik <codingeinstein@gmail.com>
2023-04-23 20:28:02 +0100

    add routes.py

---
+++ server/src/routes.py
@@ -0,0 +1,94 @@
+import os
+import torch
+
+from src import app
+from src.utils import get_tracks_from_playlist
+from src.models import AutoEncoder
+from src.pipelines import AudioPipeline, UrlToBytesToLatent
+from src.algorithm import Algorithm, Playlist, Track
+
+from flask import abort, request, Response, jsonify
+
+import requests
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+
+
+spotify_client = spotipy.Spotify(
+    client_credentials_manager=SpotifyClientCredentials())
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+net = AutoEncoder()
+net.load_state_dict(torch.load("checkpoint/checkpoint.pt",
+                    map_location=DEVICE)["state_dict"])
+net.requires_grad_(False)
+net.eval()
+net.to(DEVICE)
+
+audiopipeline = AudioPipeline()
+audiopipeline.to(DEVICE)
+
+tolatentpipeline = UrlToBytesToLatent(net=net, pipeline=audiopipeline)
+
+
+@app.route("/")
+def index():
+    return "v1"
+
+
+@app.route("/isalive")
+def isalive():
+    return Response(200)
+
+
+@app.route("/", methods=["POST"])
+def predict():
+    json = request.get_json()
+
+    anchor_id = json["anchor"]
+    playlist_id = json["playlist"]
+
+    with torch.no_grad():
+        anchor_id = request.args.get("anchor")
+        number_of_tracks, tracks = get_tracks_from_playlist(
+            spotify_client, playlist_id)
+
+        if number_of_tracks >= (int(os.environ.get("MAX_NUMBER_OF_TRACKS")) or 100):
+            abort(400, "Number of tracks in the playlist exceeds the limit")
+
+        track_ids = [track["id"] for track in tracks]
+        track_preview_urls = [track["preview_url"] for track in tracks]
+
+        tracks = zip(track_ids, track_preview_urls)
+
+        playlist = Playlist(pipeline=tolatentpipeline, tracks=[])
+
+        for id, preview_url in tracks:
+
+            track = Track(track_id=id,
+                          track_url=preview_url,
+                          is_anchor=id == anchor_id)
+
+            playlist.add_track(track)
+
+        algorthm = Algorithm(playlist)
+
+        sorted_playlist = algorthm.sort()
+
+        return jsonify({
+            "predictions": [track.track_id for track in sorted_playlist.tracks]
+        })
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return f"Could not complete Request. {error}"
+
+
+@app.errorhandler(404)
+def notfound(error):
+    return f"Not found."
